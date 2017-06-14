a351ffdc754d2b93f49380a2edf2308d265d7913
Robby <ta7382@gmail.com>
2017-06-15 00:30:49 +0800

    add textarea for lyricfs

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,27 +1,35 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { Example } from '../commons/lyrics/example';
+
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.css'],
+  providers: [Example]
 })
 export class VideoComponent implements OnInit {
 
-  public hidden: boolean = true;
-  state: Number;
-  ctime: Number = 0;
-  player: YT.Player;
-  id: string = 'https://www.youtube.com/watch?v=qIF8xvSA0Gw';
+  hidden: boolean = true;
   ytEvent: any;
-  iframe: any;
-
-  constructor() {
-
+  player: YT.Player;
+  //id: string = 'https://www.youtube.com/watch?v=qIF8xvSA0Gw';
+  id: string = 'https://www.youtube.com/watch?v=Z09BFrcSewE';
+  title: string;
+  state: Number = 0;
+  ctime: Number = 0;
+  quality: string;
+  ex: string = "";
+  ex_res: string = "";
+  constructor(private example: Example) {
+    this.ex = example.demo;
+    //this.ex_res = example.demo_res;
   }
 
   private showCurrentTime() {
     setInterval(() => {
       this.ctime = this.player.getCurrentTime();
+      this.quality = this.player.getPlaybackQuality();
     }, 1)
   }
 
@@ -30,41 +38,32 @@ export class VideoComponent implements OnInit {
   }
 
   onStateChange(event) {
-
-    let iframe = document.querySelector("iframe");
-    iframe.width = "100%";
-    iframe.height = "100%";
-
     this.ytEvent = event.target;
     this.state = event.data;
+
     if (this.state == 1) {
       this.hidden = false;
-      this.showCurrentTime();
+      this.title = this.ytEvent.getVideoData().title;
+      this.showCurrentTime();    //ytp-title-link
     }
   }
 
   changeSource() {
-    var id = this.id.split('?v=')[1];
-    let iframe = document.querySelector("iframe");
-    iframe.src = `https://www.youtube.com/embed/${id}?enablejsapi=1&origin=http%3A%2F%2Flocalhost%3A4200&widgetid=1`;
-    iframe.onload = () => {
-      this.playVideo();
-    };
+    this.player.loadVideoById(this.id.split('?v=')[1]);
   }
 
   savePlayer(player) {
     this.player = player;
-    console.log(player);
 
-    this.playVideo();
-  }
+    var iframe = this.player.getIframe();
+    iframe.width = "100%";
+    iframe.height = "100%";
 
-  playVideo() {
     this.player.playVideo();
   }
 
-  pauseVideo() {
-    this.player.pauseVideo();
+  entry() {
+    var arr = this.ex.split('\n');
+    this.ex_res = this.ex_res + arr[0] + '\n';
   }
-
 }
